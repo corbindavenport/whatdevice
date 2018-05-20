65c477b5150da4d5de9f179bc6105b51a1dc5587
Corbin Davenport <me@corbin.io>
2018-05-19 22:18:46 -0400

    Add service worker

--- js/stats.js
+++ js/stats.js
@@ -522,3 +522,42 @@ $('.modal').on('hide.bs.modal', function (e) {
 		window.location.hash = "";
 	}
 });
+
+// Service worker
+var newWorker;
+
+$(document).on("click", ".pwa-refresh", function() {
+	newWorker.postMessage({ action: 'skipWaiting' });
+});
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register("/sw.js").then(reg => {
+		reg.addEventListener("updatefound", () => {
+			// A wild service worker has appeared in reg.installing!
+			newWorker = reg.installing;
+			newWorker.addEventListener("statechange", () => {
+				// Has network.state changed?
+				switch (newWorker.state) {
+					case "installed":
+						if (navigator.serviceWorker.controller) {
+							// new update available
+							$(".pwa-refresh").show();
+						} else {
+							// no update available
+							console.log("No service worker update available.");
+						}
+						break;
+				}
+			});
+		});
+	});
+
+	var refreshing;
+	
+	navigator.serviceWorker.addEventListener("controllerchange", function () {
+		console.log("controllerchange");
+		if (refreshing) return;
+		window.location.reload();
+		refreshing = true;
+	});
+}
