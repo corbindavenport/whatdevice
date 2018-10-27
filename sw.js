c1f816c65ddadb9ee9fa471f8e025d9e5a2b0907
Corbin Davenport <me@corbin.io>
2018-10-27 11:46:44 -0400

    Add Bootswatch and Iconfinder credits to About page

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 //This is the "Offline copy of pages" service worker
-var cacheVersion = "7";
+var cacheVersion = "8";
 
 //Install stage sets up the index page (home page) in the cahche and opens a new cache
 self.addEventListener('install', function(event) {
